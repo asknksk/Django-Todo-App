76c2620cdaa45af53df3b5e73b32417dd3fc00dd
Askin <asknkskk@gmail.com>
2022-08-29 20:57:48 +0300

    todo list completed

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo   
+from .forms import TodoForm
 # Create your views here.
 
 def home(request):
-    return HttpResponse("deneme")
+    todos = Todo.objects.all()
+    context = {
+        'todos' : todos
+    }
+    return render(request, "todoApp/home.html", context)
+
+def todo_create(request):
+    form = TodoForm()
+
+    context = {
+        "form" : form
+    }
+    return render(request, "todoApp/todo_add.html", context)
