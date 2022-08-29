bc9d766b836672779e99ca3f39ebe50e217d4008
Askin <asknkskk@gmail.com>
2022-08-29 22:47:47 +0300

    delete and update finished

--- todoApp/views.py
+++ todoApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo   
 from .forms import TodoForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -20,6 +21,7 @@ def todo_create(request):
         form = TodoForm(request.POST)
         if form.is_valid:
             form.save()
+            messages.success(request, "Todo created succesfully")
             return redirect('home')
     context = {
         "form" : form
@@ -30,8 +32,27 @@ def todo_update(request, id):
     todo = Todo.objects.get(id=id)
     form = TodoForm(instance=todo)
 
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+
     context = {
         "todo" : todo,
         "form" : form
     }
     return render(request, "todoApp/todo_update.html", context)
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    
+    if request.method == "POST":
+        todo.delete()
+        messages.warning(request, "Todo deleted!")
+        return redirect("home")
+
+    context = {
+        'todo' : todo
+    }
+    return render(request, "todoApp/todo_delete.html", context)
